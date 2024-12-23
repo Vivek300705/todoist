20dba6f36a700d0958ff57c05c447a168faa17c3
VIVEK KUMAR SULANIYA <135797974+Vivek300705@users.noreply.github.com>
2024-12-23 23:52:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "https://todoist-potb.vercel.app/", // Replace with your frontend URL
+    origin: "https://todoist-potb.vercel.app", // Replace with your frontend URL
     credentials: true, // Include credentials if using cookies or authentication headers
   })
 );
